7731da710536f1682e0c8aa7ede1c85dcaa3aece
Hugo Avila <53014804+avilaHugo@users.noreply.github.com>
2024-12-02 22:58:13 -0300

    day 2 pt 2 working.

--- 2024/day_2/day_2.hs
+++ 2024/day_2/day_2.hs
@@ -2,21 +2,42 @@ parseIntTable :: String -> [[Int]]
 parseIntTable content =
     map (map (\x -> read x :: Int)) $ map words $ lines content
 
-predicateSequence :: (Int -> Int -> Bool) -> [Int] -> Bool
-predicateSequence predicate xs = foldl1 (&&) $ map (uncurry predicate) $ zip xs (tail xs)
+toPairs :: [Int] -> [(Int, Int)]
+toPairs xs = zip xs $ tail xs
 
-isIncOrDec :: [Int] -> Bool
-isIncOrDec xs = foldl1 (||) $ map (\p -> predicateSequence p xs) $ [(<=), (>=)]
+checkIsSorted :: (Int -> Int -> Bool) -> [Int] -> [Bool]
+checkIsSorted pred xs = map (uncurry pred) $ toPairs xs
 
-isChangingBy :: (Int -> Bool) -> [Int] -> Bool
-isChangingBy p xs = foldl1 (&&) $ map (p) $ map (abs . uncurry (-)) $ zip xs (tail xs)
+checkDiff :: (Int -> Bool)-> [Int] -> [Bool]
+checkDiff pred xs = map (pred . abs . uncurry (-)) $ toPairs xs
 
-isValidRecord :: [Int] -> Bool
-isValidRecord x = isIncOrDec x && isChangingBy (\y -> y > 0 && y <= 3) x
+applyFilters :: [Int] -> [[Bool]]
+applyFilters xs = map (\x -> x xs) [checkIsSorted (<=), checkIsSorted (>=), checkDiff (\x -> (0 < x) && (x <= 3))]
 
+part1Filter :: [Int] -> Bool
+part1Filter xs = (\[a, b, c] -> (a || b) && c) $ (map and . applyFilters) xs
+
+removeNth :: Int -> [a] -> [a]
+removeNth n xs = take n xs ++ drop (n + 1) xs
+
+errorFilter :: Int -> [Int] -> Bool
+errorFilter _ [] = False
+errorFilter x xs
+    | part1Filter (removeNth x xs) = True
+    | x >= (length xs) = False
+    | otherwise = errorFilter (x+1) xs
+
+-- Solve day 2 pt 1 
 day2Pt1 :: String -> IO ()
 day2Pt1 filename = do
     content <- readFile filename
-    let result = length $ filter isValidRecord $ parseIntTable content
+    let result = sum $ map fromEnum $ map part1Filter $ parseIntTable content
+    print result
+
+-- Solve day 2 pt 2
+day2Pt2 :: String -> IO ()
+day2Pt2 filename = do
+    content <- readFile filename
+    let result = sum $ map (fromEnum . (errorFilter (-1))) $ parseIntTable content
     print result
 
