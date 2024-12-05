dab66173af2695403c7172bf49b733390731715e
Hugo Avila <53014804+avilaHugo@users.noreply.github.com>
2024-12-04 21:04:15 -0300

    day 4 pt2

--- 2024/day_4/day_4.sh
+++ 2024/day_4/day_4.sh
@@ -8,10 +8,24 @@ rot() (
     done
     printf '%s\n' "${new[@]}"
 )
+seeds() {
+    (while read f; do grep --label=$((c++)) -Honb A <<< $f ; done < $1) \
+        | tr ':' ' ' | cut -d' ' -f1,3
+}
+pos() { echo '${a[$((i'"${1}"'))]:$((j'"${2}"')):1}'; }
 
+# Usage: $ this.sh input.txt
 ( # PT 1
     cat $1 | tee >(rev)                   # <->
     rot '$j' $1 | tee >(rev)              # v|^
     rot '$((i-j))' $1 | tee >(rev)        # ./°
     rot '$((i-j))' <(rev $1) | tee >(rev) # °\.
 ) | grep -o XMAS | wc -l
+
+( # PT 2
+    readarray -t a < $1
+    while read -r i j ;do
+        eval echo "$(pos -1 -1)A$(pos +1 +1)@$(pos +1 -1)A$(pos -1 +1)" 
+    done < <(seeds $1) 
+) | grep -Pc '(SAM|MAS)@(SAM|MAS)'
+
